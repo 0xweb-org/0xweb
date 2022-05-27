c849ba3968a08d644c074a50ac4be8c5d20da1b8
Alex Kit <alex.kit@atmajs.com>
2022-05-27 17:32:48 +0200

    publish: fix (path) *nix

--- src/utils/$path.ts
+++ src/utils/$path.ts
@@ -3,6 +3,6 @@ import { class_Uri } from 'atma-utils';
 export namespace $path {
     const __root = __dirname.replace(/[\\\/](lib|src).*$/, '');
     export function resolve(path: string) {
-        return class_Uri.combine('file://', __root, path);
+        return `file://` + class_Uri.combine(__root, path);
     }
 }
